a1fa6e15613805dec6992e09c65caa21ae8c6547
Joshua Fermin <Joshuaf91@gmail.com>
2016-12-20 15:13:34 -0500

    did filter evens problem

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,16 @@
 const add = (a, b) => a + b;
 
+const filterEvens = ([...args])=>{
+	return args.reduce((a,b)=>{
+		if(typeof b === 'number' && b % 2 === 0){
+			a.push(b);
+			return a;
+		}
+		return a;
+	}, [])
+}
+
 module.exports = {
-  add: add
+  add,
+  filterEvens,
 };
--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,14 +1,25 @@
 const expect = require('chai').expect;
-const add = require('../js/numbers-funcs').add;
+const func = require('../js/numbers-funcs');
 
 describe('numbers tests', () => {
+
   it('add() should add two numbers', () => {
-    var result1 = add(1, 2);
-    var result2 = add(5, 11);
-    var result3 = add(-10, 33);
+    var result1 = func.add(1, 2);
+    var result2 = func.add(5, 11);
+    var result3 = func.add(-10, 33);
     expect(result1).equal(3);
     expect(result2).equal(16);
     expect(result3).equal(23);
   });
   //Add more tests here:
+  //filterEvens takes an array (that could include any data type) and returns an array of only the even numbers
+  it('filterEvens() should remove odd numbers', ()=>{
+  	var result1 = func.filterEvens([{1:2, hello: "what"}, 2,9,10,12]);
+  	expect(result1).have.members([2,10,12])
+  	
+  	// reppetiton on next two lines 
+  	expect(result1).to.be.a('array');
+  	expect(result1).to.be.instanceof(Array);
+
+  })
 });
